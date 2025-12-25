4096d52310ec42490460c2e1dae20bd0be1fd6fb
Bedul <151714133+gggrc@users.noreply.github.com>
2025-12-25 22:14:40 +0700

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.js',
-                'resources/css/app.css',
-            ],
-            buildDirectory: 'build',
-            refresh: false,
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+            buildDirectory: 'build', 
         }),
-        vue(),
     ],
-
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-        },
-    },
-})
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    }
+});
